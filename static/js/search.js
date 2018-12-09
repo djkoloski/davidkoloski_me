c18f5589013c131f5309bcd3b337b3c0c4e57598
David Koloski <djkoloski@gmail.com>
2018-12-09 17:15:53 -0500

    Add search

---
+++ static/js/search.js
@@ -0,0 +1,53 @@
+function debounce(func, wait) {
+    return () => {
+        let args = arguments
+        if (this.timeout != null) {
+            clearTimeout(this.timeout)
+            this.timeout = null
+        }
+        this.timeout = setTimeout(() => {
+            this.timeout = null
+            func.apply(args)
+        }, wait)
+    }
+}
+
+class SearchBox {
+    constructor(index, inputNode) {
+        this.index = index
+
+        this.inputNode = inputNode
+        this.inputNode.addEventListener('input', debounce((event) => {
+            this.update()
+        }, 250))
+
+        this.resultsNode = document.createElement('div')
+        this.resultsNode.id = 'search-results'
+        this.inputNode.parentNode.insertBefore(this.resultsNode, this.inputNode.nextSibling)
+    }
+
+    update() {
+        let results = this.index.search(this.inputNode.value, {expand: true})
+        while (this.resultsNode.firstChild) {
+            this.resultsNode.removeChild(this.resultsNode.firstChild)
+        }
+        for (let result of results) {
+            let resultTextNode = document.createTextNode(result.doc.title)
+
+            let resultLinkNode = document.createElement('a')
+            resultLinkNode.href = result.ref
+            resultLinkNode.appendChild(resultTextNode)
+
+            let resultNode = document.createElement('a')
+            resultNode.href = result.ref
+            resultNode.appendChild(resultLinkNode)
+
+            this.resultsNode.appendChild(resultNode)
+        }
+    }
+}
+
+window.onload = () => {
+    const index = elasticlunr.Index.load(window.searchIndex)
+    new SearchBox(index, document.getElementById('search-input')) 
+}
